f9133a3d8e569281a72d90b5a7e7dd3c01154116
Augusto Fonseca Guerra <49533351+LimeiraGuerra@users.noreply.github.com>
2021-01-19 16:56:03 -0300

    Mudancas nos cards dos jobs

--- src/components/JobCards/index.js
+++ src/components/JobCards/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col, Row, Card} from 'antd';
 
-//import './style.css';
+import './style.css';
 
 const JobCardsComponent = ({ jobs }) => {
     console.log(jobs)
@@ -9,7 +9,8 @@ const JobCardsComponent = ({ jobs }) => {
         <Row gutter={16} style={{padding: '0 2%'}}>
             {jobs.map((job, i) => (
                 <Col key={i} span={8} style={{marginTop: '1%'}}>
-                    <Card title="Card title">
+                    <Card title={job.company.name}>
+                        <h3>Descrição:</h3>
                         {job.description}
                     </Card>
                 </Col>
